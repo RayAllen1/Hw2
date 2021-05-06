894d08a43f6f98dd33bfd8158dbea8a91e59f896
allen <steakwang0716@hotmail.com>
2021-05-06 22:30:08 +0800

    Add

--- random_selector/main.js
+++ random_selector/main.js
@@ -2,11 +2,11 @@ $(function(){
     $("input").on("click",function(){
         // alert("HI");
         // $("h1").text("hello");
-
+        console.log(1);
         let numberOfListItem = $("#choices li").length;
         let randomChildNumber = Math.floor(Math.random()*numberOfListItem);
         $("h1").text($("#choices li").eq(randomChildNumber).text());
-        
+        $("#pic").html($("#choices li").eq(randomChildNumber).find('img').clone());
         
     });
 
